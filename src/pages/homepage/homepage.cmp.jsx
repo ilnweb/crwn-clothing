8d2aa6b2a6575e09f43c5a920abfd90684f4f11b
iliyan <iliyan.tsachev@ilnweb.com>
2019-10-27 13:54:30 +0100

    broken our homepage in to smaller components, finshed styling menu ites componenet

---
+++ src/pages/homepage/homepage.cmp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.scss';
+
+// import MenuItem from '../../components/menu-item/menu-item.cmp';
+import Directory from '../../components/directory/directory.cpm';
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
