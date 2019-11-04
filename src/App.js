937f9e99efdced61137c2df44bb66bb568af3002
iliyan <iliyan.tsachev@ilnweb.com>
2019-11-04 14:05:15 +0100

    implemented firebase utils, includet ability to store authenticated user into firestore database

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomePage from './pages/homepage/homepage.cmp';
 import ShopPage from './pages/shop/shop.cmp';
 import SignInAndSignUpPage from './pages/signIn-signUp/signIn-signUp.cmp';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 
@@ -25,10 +25,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth=auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => { 
+            console.log(this.state);
+          });
+        });
+      }
+      this.setState({ currentUser: userAuth });
+    });
   }
 
   componentWillUnmount() {
--- src/pages/signIn-signUp/signIn-signUp.cmp.jsx
+++ src/pages/signIn-signUp/signIn-signUp.cmp.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './signIn-signUp.scss';
 import SignIn from '../../components/sign-in/sign-in.cmp';
+import SignUp from '../../components/sign-up/sign-up.cpm';
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
